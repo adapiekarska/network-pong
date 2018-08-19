990491d8d49e6f4cf0c6eed82213287c7f8de841
Ada <adapiekarska97@gmail.com>
2018-08-19 14:26:26 +0200

    Add example test

--- tests/tests.py
+++ tests/tests.py
@@ -1,1 +1,11 @@
 import pytest
+
+from game_client import Client
+
+# TODO how to test both client and server with their blocking methods in the same script?
+
+
+def test_client():
+    with pytest.raises(SystemExit):
+        c = Client()
+        c.start(False)
